46b322189f1d9cbaedad2546593ae9b5f36121c7
Cptkeller <67399081+Cptkeller@users.noreply.github.com>
2020-06-28 22:34:26 +0200

    Cptdumd der 2te

---
+++ Assets/Combat.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class Combat : MonoBehaviour
+{
+    //animator für animation
+    //Transform als referez für Attack und Player Pos
+    // Attack floats: range und attack dmg
+    //AoeAttack floats: Aoerange und Aoe DMG
+
+    public Animator animator;
+    public Transform PlayerPos;
+    public Transform attackPoint;
+    public Transform AoeRange;
+    public LayerMask enemyLayers;
+    public float range = 0.5f;
+    public float attackDamage = 40f;
+   
+ 
+        Vector2 sett;
+    
+    //-------------------------------------
+
+        //Update void für check per frame 
+        //If question für Attack
+        //If question für AoeAttack
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Attack();
+        }
+        
+        
+
+        
+        //-------------------------------------
+       
+
+        //Definiton für attack
+        void Attack()
+        {
+            animator.SetTrigger("kick");
+           Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, range, enemyLayers);
+
+            foreach (Collider2D enemy in hitEnemies)
+            {
+                //enemy.GetComponent<EnemyDmg>().DealDamage(attackDamage);
+            }
+    }
+
+        //-------------------------------------
+
+        //Definiton für AoeAttack
+        
+
+    }
+         //-------------------------------------
+
+
+
+        //range indicator für Attack point und Player pos.
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+        
+        Gizmos.DrawWireSphere(attackPoint.position, range);
+        Gizmos.DrawWireCube(PlayerPos.position, new Vector2( 1, 0 ));
+    }
+
+        //-------------------------------------
+}
---
+++ Assets/EnemyDmg.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class EnemyDmg : MonoBehaviour
+{
+
+    //floats für Health und max health
+    public float Health;
+    public float maxHealth;
+    //-------------------------------------
+
+
+    // Helath und maxHealth gleichgesetzt zum start
+    void Start()
+    {
+        Health = maxHealth;
+    }
+    // Funktion für DealDmg als float
+        //tot checken jede sek wäre waste, daher bei jedem hit.
+
+
+    public void DealDamage(float damage)
+    {
+        Health -= damage;
+        CheckDeath();
+    }
+    //-------------------------------------
+
+
+    //checkt ob der charakter overheald
+        // wenn ja setzte health auf max health
+    private void CheckOverheal()
+
+    {
+        if(Health < maxHealth)
+        {
+            Health = maxHealth;
+
+             
+        }
+
+
+    }
+    //-------------------------------------
+
+
+    //checkt death defnition
+    //wenn health kleiner gleich 0 dann kill
+    private void CheckDeath()
+    {
+        if (Health <= 0)
+
+        {
+
+            Destroy(gameObject);
+        }
+
+
+    }
+
+
+}
